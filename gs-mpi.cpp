3749c5612cf040a82bc164d5e0b2cf81dd0b10e1
melih mutlu <m.melihmutlu@gmail.com>
2016-06-04 15:32:17 +0300

    master proc

--- gs-mpi.cpp
+++ gs-mpi.cpp
@@ -10,8 +10,11 @@ int master = 0;	 // rank of master processor
 
 MPI_Comm_rank (MPI_COMM_WORLD, &rank); /* get current process id */
 MPI_Comm_size (MPI_COMM_WORLD, &size); /* get number of processes */
-
+int number = 0;
 if(rank == master){
+	int women = (size-1)/2; // begining of the woman processors'rank
+	int count = size-1;
+	int flag = 0;
 	/* Read input file */
 	ifstream file ; 
 	file.open(argv[1]);
@@ -23,8 +26,29 @@ if(rank == master){
 			file>> e ;
 			MPI_Send(&e, 1, MPI_INT, i, 0, MPI_COMM_WORLD);	// Sends preference lists
 		}
-
 	MPI_Barrier(MPI_COMM_WORLD); 	// Waits until all processors recieves the data
+
+	while(!flag){
+		MPI_Iprobe( MPI_ANY_SOURCE , 0 , MPI_COMM_WORLD, &flag, MPI_STATUS_IGNORE);
+		if(flag){
+			MPI_Recv(&number, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			// If woman processes confirms that they're engaged
+			if(number == terminate){
+				women--;	// Decrease number of women that are not engaged yet
+			}
+		}
+		flag = 0;
+		// If all women are engaged
+		if(women == 0){
+			// Send termination signal to all processes 
+			for(int i=1 ; i<=size-1; i++){
+				MPI_Send(&terminate, 1, MPI_INT, i , 0, MPI_COMM_WORLD);
+			}
+
+
+		MPI_Finalize(); // Finalize the master 
+		break;
+	}
 }else if(rank <= (size-1)/2){ // Men
 	vector<int> prefs;	// Preference list of men
 
@@ -33,7 +57,6 @@ if(rank == master){
 		MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 		prefs.push_back(number + (size-1)/2 );
 	}
-
 	MPI_Barrier(MPI_COMM_WORLD);	//  Waits until all processors recieves the data
 }else{	// Women
 	vector<int> prefs;	// Preference list of men
@@ -43,7 +66,6 @@ if(rank == master){
 		MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 		prefs.push_back(number);
 	}
-
 	MPI_Barrier(MPI_COMM_WORLD);	//  Waits until all processors recieves the data
 
 
